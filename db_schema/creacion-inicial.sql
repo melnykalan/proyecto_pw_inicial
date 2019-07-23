42ccc959483f0a152a6759404fca3bb8411ea298
yamilaBacchini <yamila.bacchini@gmail.com>
2019-07-23 00:09:56 -0300

    actualizado script de creacion de DB

--- db_schema/creacion-inicial.sql
+++ db_schema/creacion-inicial.sql
@@ -18,6 +18,62 @@ USE `odontologia`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `especialidades`
+--
+
+DROP TABLE IF EXISTS `especialidades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `especialidades` (
+  `idespecialidad` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`idespecialidad`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `especialidades`
+--
+
+LOCK TABLES `especialidades` WRITE;
+/*!40000 ALTER TABLE `especialidades` DISABLE KEYS */;
+INSERT INTO `especialidades` VALUES (1,'ortodoncia'),(2,'endodoncia'),(3,'general'),(4,'cirujia'),(5,'infantiles');
+/*!40000 ALTER TABLE `especialidades` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profesionales`
+--
+
+DROP TABLE IF EXISTS `profesionales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `profesionales` (
+  `idprofesional` int(11) NOT NULL AUTO_INCREMENT,
+  `dni` varchar(45) NOT NULL,
+  `direccion` varchar(200) DEFAULT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `telefono` varchar(45) DEFAULT NULL,
+  `idespecialidad` int(11) NOT NULL,
+  PRIMARY KEY (`idprofesional`),
+  UNIQUE KEY `idprofesional_UNIQUE` (`idprofesional`),
+  UNIQUE KEY `dni_UNIQUE` (`dni`),
+  KEY `idespecialidad_idx` (`idespecialidad`),
+  CONSTRAINT `idespecialidad` FOREIGN KEY (`idespecialidad`) REFERENCES `especialidades` (`idespecialidad`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profesionales`
+--
+
+LOCK TABLES `profesionales` WRITE;
+/*!40000 ALTER TABLE `profesionales` DISABLE KEYS */;
+/*!40000 ALTER TABLE `profesionales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `usuarios`
 --
 
@@ -32,8 +88,22 @@ CREATE TABLE `usuarios` (
   `admin` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `usuario_UNIQUE` (`usuario`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (2,'admin','21232f297a57a5a743894a0e4a801fc3',0,1);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'odontologia'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -44,4 +114,4 @@ CREATE TABLE `usuarios` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-07-22 19:49:25
+-- Dump completed on 2019-07-23  0:07:48
